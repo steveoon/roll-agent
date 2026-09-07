7bca4a88fa4f6c2b17fbc6bd02499023a3c20316
steveoon <rsw_mmo@hotmail.com>
2026-09-07 14:39:54 +0800

    fix(distribution): stop validating consumed CMD pointer characters

--- scripts/distribution/install.ps1
+++ scripts/distribution/install.ps1
@@ -155,7 +155,8 @@ if not defined version goto invalid
 rem Validate data using delayed expansion; never interpolate unchecked file contents into CMD syntax.
 setlocal EnableDelayedExpansion
 set "ROLL_UNSAFE_VERSION=!version!"
-for %%C in (0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z . + -) do set "ROLL_UNSAFE_VERSION=!ROLL_UNSAFE_VERSION:%%C=!"
+rem SET removes empty variables; do not run further substitutions once all characters are consumed.
+for %%C in (0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z . + -) do if defined ROLL_UNSAFE_VERSION set "ROLL_UNSAFE_VERSION=!ROLL_UNSAFE_VERSION:%%C=!"
 if defined ROLL_UNSAFE_VERSION goto invalid
 set "ROLL_VERSION_VALID="
 for %%D in (0 1 2 3 4 5 6 7 8 9) do if "!version:~0,1!"=="%%D" set "ROLL_VERSION_VALID=1"
